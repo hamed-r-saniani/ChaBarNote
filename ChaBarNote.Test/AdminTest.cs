da6f7b11cfcc9052464acf7e21a837d67b13b7be
hamed-r-saniani <hamed.r.saniani@outlook.com>
2021-08-12 15:32:08 +0430

    Add project files.

---
+++ ChaBarNote.Test/AdminTest.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using WebApi;
+
+namespace ChaBarNote.Test
+{
+    [TestClass]
+    public class AdminTest
+    {
+        private HttpClient _client;
+        public AdminTest()
+        {
+            var server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+            _client = server.CreateClient();
+
+        }
+
+
+        [TestMethod]
+        public void PostLoginTest()
+        {
+            var request = new HttpRequestMessage(new HttpMethod("POST"), $"/api/Admin/Login");
+
+            var response = _client.SendAsync(request).Result;
+
+            Assert.AreEqual(HttpStatusCode.UnsupportedMediaType, response.StatusCode);
+        }
+
+
+        [TestMethod]
+        public void PostAddUserTest()
+        {
+            var request = new HttpRequestMessage(new HttpMethod("POST"), $"/api/Admin/PostAddUser");
+
+            var response = _client.SendAsync(request).Result;
+
+            Assert.AreEqual(HttpStatusCode.UnsupportedMediaType, response.StatusCode);
+        }
+    }
+}
---
+++ ChaBarNote.Test/FolderTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using WebApi;
+
+namespace ChaBarNote.Test
+{
+    [TestClass]
+    public class FolderTest
+    {
+        private HttpClient _client;
+        public FolderTest()
+        {
+            var server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+            _client = server.CreateClient();
+
+        }
+
+        [TestMethod]
+        public void DeleteFolderTest()
+        {
+            var request = new HttpRequestMessage(new HttpMethod("POST"), $"/api/Folder/DeleteFolder");
+
+            var response = _client.SendAsync(request).Result;
+
+            Assert.AreEqual(HttpStatusCode.UnsupportedMediaType, response.StatusCode);
+        }
+    }
+}
